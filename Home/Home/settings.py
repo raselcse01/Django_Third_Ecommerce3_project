47173a9193f6df1ecc054868ff2c1789ae6732b0
Md.Rasel Hossain <72226460+raselcse01@users.noreply.github.com>
2023-06-02 00:11:54 +0600

    Update settings.py

--- Home/Home/settings.py
+++ Home/Home/settings.py
@@ -188,8 +188,8 @@ CKEDITOR_CONFIGS = {
     }
 }
 
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'rasel.vai0058@gmail.com'
-EMAIL_HOST_PASSWORD = 'tlty iums cnjo hcjd'
+EMAIL_USE_TLS =
+EMAIL_HOST = ''
+EMAIL_PORT = 
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
